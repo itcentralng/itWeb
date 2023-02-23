d3449f2fd92579e95d18ee368da2a841013acbc7
Nasir Mustapha <teboy.com@gmail.com>
2023-02-23 10:29:14 +0100

    add inRoW page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,6 +6,10 @@ import HeroSection from '../components/HeroSection'
 import Startup from '../components/Startup'
 
 function HomePage() {
+  // if the url contains intRoW, then redirect to external link
+  if (window.location.href.includes('intRoW')) {
+    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLScI-3ZtEu3aNkp1lYTscSNAGHgrgjSm3BiWpcePZaZ3blHztw/viewform'
+  }
   return (
     <>
       <HeroSection/>

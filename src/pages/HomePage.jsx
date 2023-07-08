9aa1a84dcb987aab862d839dfdf241c4151a5a36
Ibrahim <i.aliyu019@gmail.com>
2023-07-08 17:01:30 +0100

    clean up

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import Advert from '../components/Advert'
-import Card from '../components/Card'
-import Explore from '../components/Explore'
-import HeroSection from '../components/HeroSection'
-import Startup from '../components/Startup'
+import React from "react";
+import Advert from "../components/Advert";
+import Card from "../components/Card";
+import Explore from "../components/Explore";
+import HeroSection from "../components/HeroSection";
+import Startup from "../components/Startup";
+import SummerSection from "../components/SummerSection";
 
 function HomePage() {
   // if the url contains intRoW, then redirect to external link
-  if (window.location.href.includes('intRoW')) {
-    window.location.href = 'https://forms.gle/g61MD1wvikD4RRH29'
+  if (window.location.href.includes("intRoW")) {
+    window.location.href = "https://forms.gle/g61MD1wvikD4RRH29";
   }
   return (
     <>
-      <HeroSection/>
-      <Explore/>
-      <Startup/>
-      <Advert/>
-
-
+      <HeroSection />
+      <Explore />
+      <Startup />
+      <Advert />
+      <SummerSection />
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
